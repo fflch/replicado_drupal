501c05f34f3bbc645f89d17959652dc7b6a7773e
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-04-28 23:38:51 -0300

    quase funcinal

--- src/Controller/QueryOutputController.php
+++ src/Controller/QueryOutputController.php
@@ -19,6 +19,7 @@ class QueryOutputController extends ControllerBase {
    */
   public function output($route) {
 
+
     /*** 1. Conexão com o banco de dados ***/
     $config = \Drupal::service('config.factory')->getEditable('replicado.connection');
     $database_name = $config->get('database_name');
@@ -34,36 +35,53 @@ class QueryOutputController extends ControllerBase {
     putenv("REPLICADO_USERNAME={$database_user}");
     putenv("REPLICADO_PASSWORD={$database_password}");
 
-    /*** 2. Buscar query para dara rota ***/
+    /*** 2. Buscar query para rota dada ***/
     $storage = \Drupal::entityTypeManager()->getStorage('query_entity'); 
-    $nids = \Drupal::entityQuery('query_entity')->condition('route', '/'.$route, '=')->execute(); echo "<pre>"; 
+    $nids = \Drupal::entityQuery('query_entity')->condition('route', '/'.$route, '=')->execute();
 
     foreach($nids as $nid) {
+        /*só vou entrar uma vez aqui*/
+
         $entity = $storage->load($nid); 
         $query = $entity->getSql();
+
+        if (
+            (strpos(strtoupper($query), 'DROP') !== false) or
+            (strpos(strtoupper($query), 'CREATE') !== false) or
+            (strpos(strtoupper($query), 'SELECT') === false) or
+            (strpos($query, ';') !== false)
+        ) {
+          drupal_set_message(t('SQL inseguro'), 'error');
+          break;
+        }
+
         $label = $entity->label();
         $description = $entity->getDescription();
 
         /*** 3. Executa query ***/    
         $result = DB::fetchAll($query);
         $result = Uteis::utf8_converter($result);
-        // echo "<pre>"; var_dump($result); die();
+        //echo "<pre>"; var_dump($result); die();
 
-        $build['consulta'] = array(
-          '#title' => 'teste',
-          '#theme' => 'consulta',
+        return [
+          '#theme' => 'replicado',
           '#content' => $result,
           '#label' => $label,    
-          '#description' => $description,           
-        );
-        return $build;
-        break;
+          '#description' => $description,  
+          '#cache' => [
+            'max-age' => 86400,
+          ],  
+          '#attached' => [
+            'library' => [
+              'replicado/replicado',
+            ],
+          ],       
+        ];
     }
 
-    $build = [
+    return [
       '#markup' => $this->t('Consulta desconhecida'),
     ];
-    return $build;
     
   }
 

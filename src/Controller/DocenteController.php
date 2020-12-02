3118c67ffae6243b04addddbb29447986175acab
Gabriela Reis <gabrielareisg@hotmail.com>
2020-12-02 20:33:39 -0300

    Reunião Drupal Lattes

---
+++ src/Controller/DocenteController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\replicado\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Uspdev\Replicado\DB;
+use Uspdev\Replicado\Uteis;
+use Uspdev\Replicado\Lattes;
+
+/**
+ * Class QueryOutputController.
+ */
+class DocenteController extends ControllerBase {
+
+  /**
+   * Output.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function docente($codpes) {
+    /*** 1. Conexão com o banco de dados ***/
+    $config = \Drupal::service('config.factory')->getEditable('replicado.connection');
+    $database_name = $config->get('database_name');
+    $database_port = $config->get('database_port');
+    $database_host = $config->get('database_host');
+    $database_user = $config->get('database_user');
+    $database_password = $config->get('database_password');
+
+    /* TODO: Verificar se conexação ok */
+    putenv("REPLICADO_HOST={$database_host}");
+    putenv("REPLICADO_PORT={$database_port}");
+    putenv("REPLICADO_DATABASE={$database_name}");
+    putenv("REPLICADO_USERNAME={$database_user}");
+    putenv("REPLICADO_PASSWORD={$database_password}");
+    
+    $label = 'olá';
+    $description = 'hi';
+
+    $content['resumo'] = Lattes::getResumoCV($codpes);
+    $content['livros'] = Lattes::getLivrosPublicados($codpes);
+    dump($content['livros']);
+
+        return [
+          '#theme' => 'docente',
+          '#content' => $content,
+          '#label' => $label,    
+          '#description' => $description,  
+          '#cache' => [
+            'max-age' => 0,
+          ],  
+          '#attached' => [
+            'library' => [
+              'replicado/replicado',
+            ],
+          ],       
+        ];
+  }
+
+}
